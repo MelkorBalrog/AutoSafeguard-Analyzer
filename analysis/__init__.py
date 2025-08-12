6478b562daf821a0a967445bd1d9d8856b1d40a1
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-12 13:19:33 -0400

    feat: compute confusion matrix rates

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,11 +1,12 @@
 """Analysis utilities for AutoML."""
 
 from .sotif_validation import acceptance_rate, hazardous_behavior_rate, validation_time
-from .confusion_matrix import compute_metrics
+from .confusion_matrix import compute_metrics, compute_rates
 
 __all__ = [
     "acceptance_rate",
     "hazardous_behavior_rate",
     "validation_time",
     "compute_metrics",
+    "compute_rates",
 ]
