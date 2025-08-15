b89c5c010ee04f99a74103b7d30623e142c071e6
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-14 23:39:33 -0400

    Implement causal Bayesian network analysis

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -3,12 +3,14 @@
 from .sotif_validation import acceptance_rate, hazardous_behavior_rate, validation_time
 from .confusion_matrix import compute_metrics, compute_metrics_from_target
 from .safety_case import SafetyCase, SafetyCaseLibrary
+from .causal_bayesian_network import CausalBayesianNetwork
 
 __all__ = [
     "acceptance_rate",
     "hazardous_behavior_rate",
     "validation_time",
     "compute_metrics",
+    "CausalBayesianNetwork",
     "SafetyCase",
     "SafetyCaseLibrary",
     "SafetyManagementToolbox",
