b985d23fd7edde454dde175407a5566512993b6a
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-15 00:16:16 -0400

    Add CBN diagram window

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -3,12 +3,15 @@
 from .sotif_validation import acceptance_rate, hazardous_behavior_rate, validation_time
 from .confusion_matrix import compute_metrics, compute_metrics_from_target
 from .safety_case import SafetyCase, SafetyCaseLibrary
+from .causal_bayesian_network import CausalBayesianNetwork, CausalBayesianNetworkDoc
 
 __all__ = [
     "acceptance_rate",
     "hazardous_behavior_rate",
     "validation_time",
     "compute_metrics",
+    "CausalBayesianNetwork",
+    "CausalBayesianNetworkDoc",
     "SafetyCase",
     "SafetyCaseLibrary",
     "SafetyManagementToolbox",
