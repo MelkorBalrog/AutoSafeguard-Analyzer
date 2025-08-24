b17e7dcb67e58a7c2e10cc6905fdd0ee3aefcfdb
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-24 19:08:32 -0400

    feat: introduce diagram renderer facade

--- mainappsrc/__init__.py
+++ mainappsrc/__init__.py
@@ -3,5 +3,6 @@
 from .AutoML import AutoMLApp
 from .page_diagram import PageDiagram
 from .fmeda_manager import FMEDAManager
+from .diagram_renderer import DiagramRenderer
 
-__all__ = ["AutoMLApp", "PageDiagram", "FMEDAManager"]
+__all__ = ["AutoMLApp", "PageDiagram", "FMEDAManager", "DiagramRenderer"]
