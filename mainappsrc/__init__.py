c5bfab8ec2e8c2a4a2fd66899bef47b5bfd2691a
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-24 10:19:26 -0400

    Update __init__.py

--- mainappsrc/__init__.py
+++ mainappsrc/__init__.py
@@ -1,5 +1,6 @@
 """Main application package initializer."""
 
 from .AutoML import AutoMLApp
+from .page_diagram import PageDiagram
 
-__all__ = ["AutoMLApp"]
+__all__ = ["AutoMLApp", "PageDiagram"]
