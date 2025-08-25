96fe140881c6770e2c0683b097c73af2883eec76
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-25 11:12:10 -0400

    Update run_automl.bat

--- bin/run_automl.bat
+++ bin/run_automl.bat
@@ -1,3 +1,21 @@
+REM # Author: Miguel Marina <karel.capek.robotics@gmail.com>
+REM # SPDX-License-Identifier: GPL-3.0-or-later
+REM #
+REM # Copyright (C) 2025 Capek System Safety & Robotic Solutions
+REM #
+REM # This program is free software: you can redistribute it and/or modify
+REM # it under the terms of the GNU General Public License as published by
+REM # the Free Software Foundation, either version 3 of the License, or
+REM # (at your option) any later version.
+REM #
+REM # This program is distributed in the hope that it will be useful,
+REM # but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM # GNU General Public License for more details.
+REM #
+REM # You should have received a copy of the GNU General Public License
+REM # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @echo off
 REM Helper to run the AutoML executable
 
