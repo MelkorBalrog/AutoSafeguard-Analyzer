c81989a7d4e380f123bbe5138d834125296f260b
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-24 22:47:01 -0400

    Update use_case_diagram_subapp.py

--- mainappsrc/subapps/use_case_diagram_subapp.py
+++ mainappsrc/subapps/use_case_diagram_subapp.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from tkinter import simpledialog
 from mainappsrc.models.sysml.sysml_repository import SysMLRepository
-from gui.architecture import UseCaseDiagramWindow
+from gui.windows.architecture import UseCaseDiagramWindow
 
 
 class UseCaseDiagramSubApp:
